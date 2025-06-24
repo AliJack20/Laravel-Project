d090c637cb1278ddf8bdbb25d2df6cbdfed18cb9
Ali Siddiqi <alisiddiqui275@gmail.com>
2025-06-24 13:43:31 +0500

    notes formatted, view, edit and cretae working

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\Note;
+use App\Models\User;
+
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        User::factory() -> create([
+            'id' => 1,
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => bcrypt('pass123.')
+
+
+        ]);
+
+        Note::factory(100)->create();
+    }
+}
